5239d852afa2ec2970ddb5e181f0bd4dfdf91163
Kyle Carter <kylcarte@gmail.com>
2013-05-23 22:09:18 -0700

    Fixed Set* Segfaults, finished open-close model of operation.

--- src/Audio/TagLib.hs
+++ src/Audio/TagLib.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE EmptyDataDecls #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
-module Audio.TagLib where
- {- Tag()
-  , AudioProperties()
+module Audio.TagLib
+  ( taglib
+  , openFile
   , TagLib (..)
-  , TLEnv (..)
-  , withFiles  , withFile
   , getTitle   , setTitle  
   , getArtist  , setArtist 
   , getAlbum   , setAlbum  
@@ -21,16 +20,10 @@ module Audio.TagLib where
   , getBitrate
   , getSampleRate
   , getChannels
-  , inBase
   ) where
 
-import Control.Applicative (Applicative(..),(<$>),(<*>))
-import Control.Monad ((>=>),ap)
-import Control.Monad.Trans.Reader (ReaderT (..),asks)
-import Data.Maybe (listToMaybe)
--}
-
-import MonadLib
+import Control.Monad.IO.Class
+import Control.Monad.State
 
 import Control.Applicative
 import qualified Control.Exception as E
@@ -38,14 +31,71 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 import Data.Typeable
 import Data.Word (Word8)
-import Foreign.C.String (CString,withCString)
+import Foreign.C.String (CString,withCString,peekCString)
 import Foreign.C.Types (CInt(..),CChar(..))
 import Foreign.Ptr (Ptr,nullPtr)
 import Foreign.Marshal.Array (lengthArray0,copyArray)
-import qualified Data.ByteString as SI
-import qualified Data.ByteString.Internal as SI
+import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+
+-- Types {{{
+
+-- | Monad for performing TagLib operations
+newtype TagLib a = TagLib
+  { unTagLib :: StateT TagLibEnv IO a
+  } deriving (Functor, Monad, Applicative, MonadIO, MonadState TagLibEnv)
+
+-- | Internal representation of an open file
+data TagLibFile = TagLibFile
+  { filePtr      :: Ptr File
+  , tagPtr       :: Ptr Tag
+  , audioPropPtr :: Ptr AudioProperties
+  }
+
+-- | A handle for an open file
+newtype FileId = FileId Integer deriving (Eq,Ord)
+
+-- | Abstract C Types
+data File
+data Tag
+data AudioProperties
+
+-- | FFI Type Synonyms
+type SetStringTag = Ptr Tag -> CString -> IO ()
+type SetIntTag = Ptr Tag -> CInt -> IO ()
+
+type GetStringTag = Ptr Tag -> IO (Ptr CChar)
+type GetIntTag = Ptr Tag -> IO CInt
+type GetIntAP = Ptr AudioProperties -> IO CInt
+
+-- }}}
+
+-- Env {{{
+
+-- | A collection of open files, and a generator for unique file ID's
+data TagLibEnv = TagLibEnv
+  { taglibFilesOpen :: M.Map FileId TagLibFile
+  , taglibNextId    :: Integer
+  }
+
+-- | A fresh Env
+initialEnv :: TagLibEnv
+initialEnv = TagLibEnv M.empty 0
+
+-- | Record modify for taglibFilesOpen
+onFilesOpen :: (M.Map FileId TagLibFile -> M.Map FileId TagLibFile)
+  -> TagLibEnv -> TagLibEnv
+onFilesOpen f e = e { taglibFilesOpen = f $ taglibFilesOpen e }
+
+-- | Record modify for taglibNextId
+onNextId :: (Integer -> Integer)
+  -> TagLibEnv -> TagLibEnv
+onNextId f e = e { taglibNextId = f $ taglibNextId e }
+
+-- }}}
+
+-- Exceptions {{{
 
 data TagLibException
   = NoSuchFileId
@@ -55,36 +105,9 @@ data TagLibException
 
 instance E.Exception TagLibException
 
-newtype TagLib a = TagLib
-  { unTagLib :: StateT TagLibEnv IO a
-  } deriving (Functor, Monad, Applicative)
+-- }}}
 
-instance BaseM TagLib IO where
-  inBase = TagLib . inBase
-
-instance StateM TagLib TagLibEnv where
-  get = TagLib get
-  set = TagLib . set
-
-data TagLibEnv = TagLibEnv
-  { taglibFilesOpen :: M.Map FileId TagLibFile
-  , taglibNextId    :: Integer
-  }
-
-initEnv :: TagLibEnv
-initEnv = TagLibEnv M.empty 0
-
-data TagLibFile = TagLibFile
-  { filePtr      :: Ptr File
-  , tagPtr       :: Ptr Tag
-  , audioPropPtr :: Ptr AudioProperties
-  }
-
-newtype FileId = FileId Integer deriving (Eq,Ord)
-
-data File
-data Tag
-data AudioProperties
+-- Main Interface {{{
 
 taglib :: TagLib a -> IO a
 taglib m = do
@@ -93,11 +116,10 @@ taglib m = do
   c_taglib_free_strings
   return res
   where
-  getOpenFilePtrs = gets $ map filePtr . M.elems . taglibFilesOpen
-  eval = evalStateT initEnv . unTagLib
+  eval = flip evalStateT initialEnv . unTagLib
   m' = do
     res <- m 
-    fs <- getOpenFilePtrs
+    fs <- openFilePtrs
     return (res,fs)
   cleanup f = do
     c_taglib_file_save f
@@ -105,7 +127,7 @@ taglib m = do
 
 openFile :: FilePath -> TagLib FileId
 openFile fp = do
-  f <- inBase $ withCString fp $ \c_path -> do
+  f <- liftIO $ withCString fp $ \c_path -> do
     c_file <- c_taglib_file_new c_path
     if (c_file == nullPtr)
     then E.throw (UnableToOpen fp) 
@@ -116,53 +138,93 @@ openFile fp = do
       else TagLibFile c_file <$>
            c_taglib_file_tag c_file <*>
            c_taglib_file_audioproperties c_file
-  i <- getNextId
-  sets_ $ \e -> e { taglibFilesOpen = M.insert i f $ taglibFilesOpen e }
+  i <- nextId
+  addNewFile i f
   return i
 
-getNextId :: TagLib FileId
-getNextId = do
-  i <- gets taglibNextId
-  sets_ $ \e -> e { taglibNextId = taglibNextId e + 1 }
+-- }}}
+
+-- Monadic Operations {{{
+
+-- | Put a new file into the Env
+addNewFile :: FileId -> TagLibFile -> TagLib ()
+addNewFile i f = modify $ onFilesOpen $ M.insert i f
+
+-- | Get a fresh FileId, maintaining the internal generator
+nextId :: TagLib FileId
+nextId = do
+  i <- fromEnv taglibNextId
+  modify $ onNextId (+1)
   return $ FileId i
 
-{-
-  c_taglib_file_tag c_file <*>
-  c_taglib_file_audioproperties c_file
+-- | Get the list of currently opened files.
+openFilePtrs :: TagLib [Ptr File]
+openFilePtrs = fromEnv $ map filePtr . M.elems . taglibFilesOpen
 
-bracket :: [CString] -> (Ptr TagLibFile -> IO a) -> IO (Maybe [a])
-bracket c_paths f = loop c_paths id
-  where
-  loop ps k = case ps of
-    []    -> fmap Just $ mapM f $ k []
-    p:ps' -> E.bracket (c_taglib_file_new p) cleanupFile $ \c_file ->
-      whenMaybe (c_file /= nullPtr) $ do
-        res <- c_taglib_file_is_valid c_file
-        whenMaybe (res /= 0) $ loop ps' (k . (c_file:))
--}
+-- | Call a function requiring the Env
+fromEnv :: (TagLibEnv -> a) -> TagLib a
+fromEnv f = TagLib $ gets f
 
+-- | Call a function requiring a file.
+--   Throws an exception should the FileId not point
+--   to a currently open file.
 fromFile :: (TagLibFile -> a) -> FileId -> TagLib a
 fromFile acc fid = do
-  mf <- M.lookup fid <$> gets taglibFilesOpen
+  mf <- M.lookup fid <$> fromEnv taglibFilesOpen
   case mf of
     Just f -> return (acc f)
-    Nothing -> inBase $ E.throw NoSuchFileId
+    Nothing -> liftIO $ E.throw NoSuchFileId
 
-gets :: (Functor m, StateM m s) => (s -> a) -> m a
-gets f = f <$> get
+-- }}}
 
-evalStateT :: (Monad m) => s -> StateT s m a -> m a
-evalStateT s m = do
-  (a,s') <- runStateT s m
-  return a
+-- FFI Wrappers {{{
 
-type SetStringTag = CString -> Ptr Tag -> IO ()
-type SetIntTag = CInt -> Ptr Tag -> IO ()
-type GetStringTag = Ptr Tag -> IO (Ptr Word8)
-type GetIntTag = Ptr Tag -> IO CInt
-type GetIntAP = Ptr AudioProperties -> IO CInt
+-- | Given a @IO@ action which expects a @Tag@ pointer and @CString@,
+--   lifts it into an @TagLib@ action, expecting @Text@.
+packStringTag :: SetStringTag -> FileId -> T.Text -> TagLib ()
+packStringTag k fid txt = do
+  c_tag <- fromFile tagPtr fid
+  liftIO $ BS.useAsCString bs $ k c_tag
+  where
+  bs :: BS.ByteString
+  bs = T.encodeUtf8 txt
 
--- File FFI ----------------------------------------------------------------{{{1
+-- | Given a @IO@ action which expects a @Tag@ pointer and @CInt@,
+--   lifts it into an @TagLib@ action, expecting a @Int@.
+packIntTag :: SetIntTag -> FileId -> Int -> TagLib ()
+packIntTag k fid int = do
+  c_tag <- fromFile tagPtr fid
+  liftIO $ k c_tag $ toEnum int
+
+-- | Given a @IO@ action which expects a @Tag@ pointer and
+--   results in a @CString@, lifts it into a @TagLib@ action,
+--   resulting in @Text@.
+unpackStringTag :: GetStringTag -> FileId -> TagLib T.Text
+unpackStringTag k fid = do
+  c_tag <- fromFile tagPtr fid
+  liftIO $ do
+    c_str <- k c_tag
+    T.pack <$> peekCString c_str
+
+-- | Given a @IO@ action which expects a @Tag@ pointer and
+--   results in a @CInt@, lifts it into a @TagLib@ action,
+--   resulting in @Int@.
+unpackIntTag :: GetIntTag -> FileId -> TagLib Int
+unpackIntTag k fid = do
+  c_tag <- fromFile tagPtr fid
+  liftIO $ fromIntegral <$> k c_tag
+
+-- | Given a @IO@ action which expects a @AudioProperties@ pointer and
+--   results in a @CInt@, lifts it into a @TagLib@ action,
+--   resulting in @Int@.
+unpackIntAP :: GetIntAP -> FileId -> TagLib Int
+unpackIntAP k fid = do
+  c_ap <- fromFile audioPropPtr fid
+  liftIO $ fromIntegral <$> k c_ap
+
+-- }}}
+
+-- File FFI {{{
 
 foreign import ccall "taglib_file_new"
   c_taglib_file_new :: CString -> IO (Ptr File)
@@ -186,54 +248,8 @@ foreign import ccall "taglib_tag_free_strings"
   c_taglib_free_strings :: IO ()
 
 -- }}}
--- FFI Wrappers ------------------------------------------------------------{{{1
 
--- | Given a @IO@ action which expects a @Tag@ pointer and @CString@,
---   lifts it into an @TagLib@ action, expecting @Text@.
-packStringTag :: SetStringTag -> FileId -> T.Text -> TagLib ()
-packStringTag k fid txt = do
-  c_tag <- fromFile tagPtr fid
-  inBase $ SI.useAsCString bs $ flip k c_tag
-  where
-  bs = T.encodeUtf8 txt
-
--- | Given a @IO@ action which expects a @Tag@ pointer and @CInt@,
---   lifts it into an @TagLib@ action, expecting a @Int@.
-packIntTag :: SetIntTag -> FileId -> Int -> TagLib ()
-packIntTag k fid int = do
-  c_tag <- fromFile tagPtr fid
-  inBase $ k (toEnum int) c_tag
-
--- | Given a @IO@ action which expects a @Tag@ pointer and
---   results in a @CString@, lifts it into a @TagLib@ action,
---   resulting in @Text@.
-unpackStringTag :: GetStringTag -> FileId -> TagLib T.Text
-unpackStringTag k fid = do
-  c_tag <- fromFile tagPtr fid
-  inBase $ do
-    c_str <- k c_tag
-    len   <- lengthArray0 0 c_str
-    bs <- SI.create len (\ dst -> copyArray dst c_str len)
-    return (T.decodeUtf8 bs)
-
--- | Given a @IO@ action which expects a @Tag@ pointer and
---   results in a @CInt@, lifts it into a @TagLib@ action,
---   resulting in @Int@.
-unpackIntTag :: GetIntTag -> FileId -> TagLib Int
-unpackIntTag k fid = do
-  c_tag <- fromFile tagPtr fid
-  inBase $ fromIntegral <$> k c_tag
-
--- | Given a @IO@ action which expects a @AudioProperties@ pointer and
---   results in a @CInt@, lifts it into a @TagLib@ action,
---   resulting in @Int@.
-unpackIntAP :: GetIntAP -> FileId -> TagLib Int
-unpackIntAP k fid = do
-  c_ap <- fromFile audioPropPtr fid
-  inBase $ fromIntegral <$> k c_ap
-
--- }}}
--- Tag Setters -------------------------------------------------------------{{{1
+-- Tag Setters {{{
 
 setTitle :: FileId ->  T.Text -> TagLib ()
 setTitle = packStringTag c_taglib_tag_set_title
@@ -257,7 +273,6 @@ setTrack :: FileId ->  Int -> TagLib ()
 setTrack = packIntTag c_taglib_tag_set_track
 
 
-
 foreign import ccall "taglib_tag_set_title"
   c_taglib_tag_set_title :: SetStringTag
 
@@ -279,7 +294,9 @@ foreign import ccall "taglib_tag_set_year"
 foreign import ccall "taglib_tag_set_track"
   c_taglib_tag_set_track :: SetIntTag
 
--- Tag Getters -------------------------------------------------------------{{{1
+-- }}}
+
+-- Tag Getters {{{
 
 getTitle :: FileId -> TagLib T.Text
 getTitle  = unpackStringTag c_taglib_tag_title
@@ -303,7 +320,6 @@ getTrack :: FileId -> TagLib Int
 getTrack  = unpackIntTag c_taglib_tag_track
 
 
-
 foreign import ccall "taglib_tag_title"
   c_taglib_tag_title :: GetStringTag
 
@@ -325,7 +341,9 @@ foreign import ccall "taglib_tag_year"
 foreign import ccall "taglib_tag_track"
   c_taglib_tag_track :: GetIntTag
 
--- AudioProperties Getters -------------------------------------------------{{{1
+-- }}}
+
+-- AudioProperties Getters {{{
 
 -- | Retrieves the duration of the given file, in seconds.
 getLength :: FileId -> TagLib Int
@@ -344,7 +362,6 @@ getChannels :: FileId -> TagLib Int
 getChannels = unpackIntAP c_taglib_audioproperties_channels
 
 
-
 foreign import ccall "taglib_audioproperties_length"
   c_taglib_audioproperties_length :: GetIntAP
 
@@ -358,82 +375,4 @@ foreign import ccall "taglib_audioproperties_channels"
   c_taglib_audioproperties_channels :: GetIntAP
 
 -- }}}
-
-{-
--- Base --------------------------------------------------------------------{{{1
-
--- | Process a computation requiring a @Ptr Tag@ and @Ptr AudioProperties@
---   using a given file, producing one result for each @FilePath@ given.
-withFiles :: [FilePath] -> TagLib a -> IO (Maybe [a])
-withFiles paths m =
-  withCStrings paths $ \ c_paths ->
-    bracket c_paths  $ 
-      buildEnv >=> evalTagLib m 
-
--- | Retrieve the @Tag@ and @AudioProperties@ pointers
---   from a @TagLibFile@ pointer.
-buildEnv :: Ptr TagLibFile -> IO TLEnv
-buildEnv c_file = TLEnv    <$>
-  c_taglib_file_tag c_file <*>
-  c_taglib_file_audioproperties c_file
-
--- | Process a computation for exactly one file, as per @withFiles@.
-withFile :: FilePath -> TagLib a -> IO (Maybe a)
-withFile path m = do
-  res <- withFiles [path] m
-  case res of
-    Just r -> return $ listToMaybe r
-    Nothing -> return Nothing
-
--- | Save any changes made to file, and free all associated memory.
-cleanupFile :: Ptr TagLibFile -> IO ()
-cleanupFile c_file  = do
-  c_taglib_file_save c_file
-  c_taglib_free_strings
-  c_taglib_file_free c_file
-
--- | For all @CString@s, each representing a @FilePath@, and a computation
---   expecting a pointer to a file, run the computation on all files,
---   if and only if all strings are valid files which are correctly opened
---   by tag_c's @taglib_file_new@.
-bracket :: [CString] -> (Ptr TagLibFile -> IO a) -> IO (Maybe [a])
-bracket c_paths f = loop c_paths id
-  where
-  loop ps k = case ps of
-    []    -> fmap Just $ mapM f $ k []
-    p:ps' -> E.bracket (c_taglib_file_new p) cleanupFile $ \c_file ->
-      whenMaybe (c_file /= nullPtr) $ do
-        res <- c_taglib_file_is_valid c_file
-        whenMaybe (res /= 0) $ loop ps' (k . (c_file:))
-
--- | Pluralized @withCString@.
-withCStrings :: [String] -> ([CString] -> IO a) -> IO a
-withCStrings ss f = loop ss id
-  where
-  loop l k = case l of
-    []   -> f $ k []
-    s:l' -> withCString s $ \c_str -> loop l' (k . (c_str:))
-
--- | Simple helper. Continue with given computation upon a condition.
-whenMaybe :: (Monad m) => Bool -> m (Maybe a) -> m (Maybe a)
-whenMaybe b m = if b
-  then m
-  else return Nothing
-
--- | Abstract Tag object.
-data Tag
-type TagP = Ptr Tag
-type SetStringTag = CString -> TagP -> IO ()
-type SetIntTag = CInt -> TagP -> IO ()
-type GetStringTag = TagP -> IO (Ptr Word8)
-type GetIntTag = TagP -> IO CInt
-
-data AudioProperties
-type APP = Ptr AudioProperties
-type GetIntAP = APP -> IO CInt
-
-data TagLibFile
-
--}
-
 
